cf313c52eaede7ddd2e0bee1a62f7d54a1c12b3a
Mehmet Akif Balcı <akif.balci@ogr.sakarya.edu.tr>
2023-12-31 17:07:34 +0300

    project end

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
-
 public class Main {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        Reader reader = new Reader("src/giris.txt");
 
-	}
+        Assigner.allProcesses = reader.read();
 
+        Assigner.run();
+
+
+
+
+    }
 }

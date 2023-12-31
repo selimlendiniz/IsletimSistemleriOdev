e408484a449dc2497368991200528bd66436ddf6
selimlendiniz <sardacevik@icloud.com>
2023-12-31 17:20:07 +0300

    runable jar file added

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        Reader reader = new Reader("src/giris.txt");
+        Reader reader = new Reader("giris.txt");
 
         Assigner.allProcesses = reader.read();
 
         Assigner.run();
-
-
-
 
     }
 }
